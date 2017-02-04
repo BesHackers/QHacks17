49aa3fb826b6694e3da5b2541833a39a7a8e1fa8
Ben-Alderson <bennyalderson@gmail.com>
2017-02-04 10:32:40 -0500

    Fix dragging on activityPage

--- js/activityPage.js
+++ js/activityPage.js
@@ -1,6 +1,5 @@
 function setProgressBar(value, total) {
   document.querySelector("#myBar").style.width = (value/total)*100 + "%";
-  console.log((value/total) + "%");
 }
 
 setProgressBar(20, 100);
@@ -11,23 +10,43 @@ function allowDrop(ev) {
 }
 
 function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
+    ev.dataTransfer.setData("parent_id", ev.target.parentNode.id);
+    ev.dataTransfer.setData("element_id", ev.target.id);
 }
 
 function drop(ev) {
   ev.preventDefault();
-  var data=ev.dataTransfer.getData("text");
+  // Don't allow drags to the same box
+  if(ev.target.id == ev.dataTransfer.getData("parent_id")) {
+    return;
+  }
+
+  var data=ev.dataTransfer.getData("element_id");
 	/* If you use DOM manipulation functions, their default behaviour it not to
      copy but to alter and move elements. By appending a ".cloneNode(true)",
      you will not move the original element, but create a copy. */
-	var nodeCopy = document.getElementById(data).cloneNode(true);
-	nodeCopy.id = "newId" + (x++); /* We cannot use the same ID */
-	ev.target.appendChild(nodeCopy);
+  if(ev.target.id != "div2") {
+    ev = {
+      target: ev.target.parentNode,
+      dataTransfer: ev.dataTransfer,
+      preventDefault: function(){},
+    };
+    drop(ev)
+  } else {
+    var nodeCopy = document.getElementById(data).cloneNode(true);
+    nodeCopy.id = "newId" + (x++); /* We cannot use the same ID */
+  	ev.target.appendChild(nodeCopy);
+  }
 }
 
 function drop1(ev){
-	ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
+  ev.preventDefault();
+  // Don't allow drags to the same box
+  if(ev.target.id == ev.dataTransfer.getData("parent_id")) {
+    return;
+  }
+
+  var data = ev.dataTransfer.getData("element_id");
 	var el = document.getElementById(data);
 	el.parentNode.removeChild(el);
 }
